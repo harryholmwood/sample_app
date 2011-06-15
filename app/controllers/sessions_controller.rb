baf69e51c05f7088a659e50a5134afcc56b40ad4
Harry Holmwood <harry@heldhand.com>
2011-06-15 12:48:51 +0100

    Added a sessions controller and a signin view

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+  def new
+    @title = "Sign in"
+  end
+
+  def create
+    user = User.authenticate( params[:session][:email], 
+                              params[:session][:password])
+
+    if user.nil?
+      flash.now[:error] = "Invalid email/password combination"	
+      @title = "Sign in"
+      render 'new'
+    else
+# sign user in and redirect to user's show page
+    end
+  end
+
+  def destroy
+  end
+
+end
