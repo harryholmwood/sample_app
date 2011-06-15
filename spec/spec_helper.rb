7d373d547efc95caeeb19853a9027f1c9db6c57f
Harry Holmwood <harry@heldhand.com>
2011-06-15 17:11:29 +0100

    Completed Signin/out

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -53,4 +53,10 @@ RSpec.configure do |config|
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
+
+#  ActiveSupport::Dependencies.clear
+
+  def test_sign_in(user)
+    controller.sign_in(user)
+  end
 end
