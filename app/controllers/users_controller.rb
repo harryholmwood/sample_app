a7428daaf0db8c14804a8915b29f4c070d4566fd
Harry Holmwood <harry@heldhand.com>
2011-06-21 13:10:24 +0100

    Can now destroy users (as admins)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -71,7 +71,7 @@ class UsersController < ApplicationController
 
   def admin_user
     user = User.find(params[:id])
-    redirect_to(root_path) if !current_user.admin?  || current_user?(user)
+    redirect_to root_path, :flash => {:error => "Cannot destroy yourself."} if !current_user.admin?  || current_user?(user)
   end
 
 end
